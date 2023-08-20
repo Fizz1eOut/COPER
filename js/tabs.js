ad3f704af02bcf524a9215cc9624927fe2674c8e
FizzleOut <fizzleout1337@gmail.com>
2023-08-20 14:44:12 +0200

    changes tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
+  function createBtns(catalog) {
+    const btns = catalog.querySelectorAll('.tabs__btn');
+    btns.forEach((el) => {
+      el.classList.remove('tabs__btn--active');
+    });
+    return btns;
+  }
+
+  function createContent(catalog) {
+    const content = catalog.querySelectorAll('.tabs__content');
+    content.forEach((el) => {
+      el.classList.remove('tabs__content--active');
+    });
+    return content;
+  }
+
   document.addEventListener('click', (e) => {
     if (e.target.classList.contains('tabs__btn')) {
       const catalog = e.target.closest('.catalog__tabs');
       const { tabsPath } = e.target.dataset;
 
-      const btns = catalog.querySelectorAll('.tabs__btn');
-      btns.forEach((el) => {
-        el.classList.remove('tabs__btn--active');
-      });
+      createBtns(catalog);
+
       e.target.classList.add('tabs__btn--active');
 
-      const content = catalog.querySelectorAll('.tabs__content');
-      content.forEach((el) => {
-        el.classList.remove('tabs__content--active');
-      });
+      createContent(catalog);
+
       catalog.querySelector(`[data-tabs-target="${tabsPath}"]`).classList.add('tabs__content--active');
     }
   });
