dc654860b50ad38ee4513e683efa451bb76b9894
FizzleOut <fizzleout1337@gmail.com>
2023-09-09 20:04:22 +0200

    addition swiper and footer

--- js/swiper.js
+++ js/swiper.js
@@ -47,3 +47,150 @@ new Swiper('.new-products', {
   // Листание
   effect: 'slide',
 });
+
+new Swiper('.hits-products', {
+  navigation: {
+    nextEl: '.hits-products__arrow.swiper-button-next',
+    prevEl: '.hits-products__arrow.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.hits-products__swiper-pagination.swiper-pagination',
+    clickable: true
+  },
+  // Управление клавиатурой
+  keyboard: {
+    // включить/выключить
+    enabled: true,
+    // Вкл/Выкл только когда слайдер в пределах вьюпорта
+    onlyInViewport: true,
+    // включить/выключить управление клавиатурой PageUp, pageDown
+    pageUpDown: true,
+  },
+
+  // Автовысота
+  autoHeihgt: true,
+  // Количство слайдов для показа
+  slidesPerView: 4,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    460: {
+      slidesPerView: 2,
+    },
+    675: {
+      slidesPerView: 3,
+    },
+    1058: {
+      slidesPerView: 4,
+    },
+    1317: {
+      slidesPerView: 5,
+    },
+  },
+
+  // Отступ между слайдами
+  spaceBetween: 10,
+  // Эффекты переключение слайдов
+  // Листание
+  effect: 'slide',
+});
+
+new Swiper('.sale-products', {
+  navigation: {
+    nextEl: '.sale-products__arrow.swiper-button-next',
+    prevEl: '.sale-products__arrow.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.sale-products__swiper-pagination.swiper-pagination',
+    clickable: true
+  },
+  // Управление клавиатурой
+  keyboard: {
+    // включить/выключить
+    enabled: true,
+    // Вкл/Выкл только когда слайдер в пределах вьюпорта
+    onlyInViewport: true,
+    // включить/выключить управление клавиатурой PageUp, pageDown
+    pageUpDown: true,
+  },
+
+  // Автовысота
+  autoHeihgt: true,
+  // Количство слайдов для показа
+  slidesPerView: 4,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    460: {
+      slidesPerView: 2,
+    },
+    675: {
+      slidesPerView: 3,
+    },
+    1058: {
+      slidesPerView: 4,
+    },
+    1317: {
+      slidesPerView: 5,
+    },
+  },
+
+  // Отступ между слайдами
+  spaceBetween: 10,
+  // Эффекты переключение слайдов
+  // Листание
+  effect: 'slide',
+});
+
+new Swiper('.popular-products', {
+  navigation: {
+    nextEl: '.popular-products__arrow.swiper-button-next',
+    prevEl: '.popular-products__arrow.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.popular-products__swiper-pagination.swiper-pagination',
+    clickable: true
+  },
+  // Управление клавиатурой
+  keyboard: {
+    // включить/выключить
+    enabled: true,
+    // Вкл/Выкл только когда слайдер в пределах вьюпорта
+    onlyInViewport: true,
+    // включить/выключить управление клавиатурой PageUp, pageDown
+    pageUpDown: true,
+  },
+
+  // Автовысота
+  autoHeihgt: true,
+  // Количство слайдов для показа
+  slidesPerView: 4,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    460: {
+      slidesPerView: 2,
+    },
+    675: {
+      slidesPerView: 3,
+    },
+    1058: {
+      slidesPerView: 4,
+    },
+    1317: {
+      slidesPerView: 5,
+    },
+  },
+
+  // Отступ между слайдами
+  spaceBetween: 10,
+  // Эффекты переключение слайдов
+  // Листание
+  effect: 'slide',
+});
