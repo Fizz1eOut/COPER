81817be1f4d6fdc27374db879fb5df5ad0feb95a
FizzleOut <fizzleout1337@gmail.com>
2023-09-24 20:05:35 +0200

    changes to favorites

--- js/favorites.js
+++ js/favorites.js
@@ -4,61 +4,104 @@ document.addEventListener('DOMContentLoaded', () => {
   const likedQuantity = document.querySelector('.liked__quantity');
 
   const printQuantity = () => {
-    let likedLength = favoritesList.children.length;
-    likedQuantity.textContent = likedLength;
-    // console.log(cardProductsList)
-    // console.log(length)
+    // лучше проверять не через children, а искать через querySelectorAll все элементы с классом .favorites__item
+    // let likedLength = favoritesList.children.length;
+    // likedQuantity.textContent = likedLength;
+
+    const { length } = favoritesList.querySelectorAll('.favorites__item');
+    likedQuantity.textContent = length;
   };
 
-  const generateLikedProduct = (img, title, liked, rating, stiker, testimonials, price, id) => {
+  const generateLikedProduct = (data) => {
     return `
-    <li class="favorites__item">
-    <article class="favorites-product" data-id="${id}">
-      <div class="favorites-product__row">
-        <div class="favorites-product__stiker favorites-product__stiker--green">${stiker}</div>
-        <button class="favorites-product__lekid product__liked">
-        ${liked}
-        </button>
-      </div>
-      <div class="favorites-product__image">
-        <img src="${img}" alt="">
-      </div>
-      <div class="favorites-product__props">
-        <span class="favorites-product__rating">
-          <img src="${rating}" alt="">
-        </span>
-        <span class="favorites-product__testimonials">${testimonials}</span>
-      </div>
-      <h3 class="favorites-product__title">
-        <a href="#">${title}</a>
-      </h3>
-      <div class="favorites-product__price">
-        ${price}
-      </div>
-      <button class="favorites-product__btn button--buy">До кошика</button>
-    </article>
-  </li>
-          `;
+      <li class="favorites__item">
+        <article class="favorites-product" data-id="${data.id}">
+          <div class="favorites-product__row">
+            <div class="favorites-product__stiker favorites-product__stiker--green">${data.stiker}</div>
+            <button class="favorites-product__lekid product__liked">
+            ${data.liked}
+            </button>
+          </div>
+          <div class="favorites-product__image">
+            <img src="${data.img}" alt="">
+          </div>
+          <div class="favorites-product__props">
+            <span class="favorites-product__rating">
+              <img src="${data.rating}" alt="">
+            </span>
+            <span class="favorites-product__testimonials">${data.testimonials}</span>
+          </div>
+          <h3 class="favorites-product__title">
+            <a href="#">${data.title}</a>
+          </h3>
+          <div class="favorites-product__price">
+            ${data.price}
+          </div>
+          <button class="favorites-product__btn button--buy">До кошика</button>
+        </article>
+      </li>
+    `;
+  };
+
+  // button - элемент кнопки, state - состояние (true - красная, false - нет); если ничего не передали в state, то по дефолту false
+  const updateButton = (button, state = false) => button.classList.toggle('active', state);
+
+  // удаляем товар из избранного; принимает id товара для удаления
+  const removeProduct = (id) => {
+    // ищем товар по id
+    const product = favoritesList.querySelector(`[data-id="${id}"]`);
+    // находим его обертку, которую мы хотим удалить
+    const wrapper = product?.closest('.favorites__item');
+    // удаляем товар и заодно проверяем, что он там точно есть через оператор `?`, если вдруг товара/обертки нет, то ошибки не будет
+    wrapper?.remove();
+    printQuantity(); // обновляем количество
+  };
+
+  // добавляем товар в избранное; data - объект с данными о товаре для добавления
+  const addProduct = (data) => {
+    favoritesList.insertAdjacentHTML('afterbegin', generateLikedProduct(data)); // создаем товар и добавляем в список
+    printQuantity(); // обновляем количество
   };
 
   likedBtn.forEach((el) => {
     el.addEventListener('click', (e) => {
-      let parent = e.target.closest('.product');
-      let { id } = parent.dataset;
-      console.log(id);
-      let stiker = parent.querySelector('.product__stiker').textContent;
-      let liked = parent.querySelector('.product__liked').innerHTML;
-      let img = parent.querySelector('.product__image img').getAttribute('src');
-      let rating = parent.querySelector('.product__rating img').getAttribute('src');
-      let testimonials = parent.querySelector('.product__testimonials').textContent;
-      let title = parent.querySelector('.product__title').textContent;
-      let price = parent.querySelector('.product-price').innerHTML;
+      const parent = e.target.closest('.product');
+      const { id } = parent.dataset;
 
-      favoritesList.insertAdjacentHTML('afterbegin', generateLikedProduct(img, title, liked, rating, stiker, testimonials, price, id));
-      console.log(favoritesList);
-      printQuantity();
+      const data = { // формируем объект со всеми данными о товаре, чтобы не передавать каждое свойство по одному
+        id, // то же самое, что id: id
+        title: parent.querySelector('.product__title').textContent,
+        price: parent.querySelector('.product-price').innerHTML,
+        stiker: parent.querySelector('.product__stiker').textContent,
+        rating: parent.querySelector('.product__rating img').getAttribute('src'),
+        img: parent.querySelector('.product__image img').getAttribute('src'),
+        liked: parent.querySelector('.product__liked').innerHTML,
+        testimonials: parent.querySelector('.product__testimonials').textContent,
+      };
+      // ----> e.target.disabled = true; <----
+      // это не работает, потому что e.target - это то, куда именно ты нажал мышкой, а внутри кнопки лежит <svg>, в котором есть еще <path>,
+      // поэтому клик может быть на любом из этих элементов, а не конкретно на <button>
 
-      e.target.disabled = true;
+      const button = e.target.closest('.product__liked'); // находим нашу реальную кнопку вверх по дереву
+
+      // button.disabled = true; // вместо этого лучше добавлять дополнительный класс для button и красить кнопку в красный цвет
+      // этот код перенесен в функцию `updateButton`, disabled тоже убран
+
+      // ищем в списке товар по селектору с id, приводим к boolean с помощью сравнения
+      const isLiked = favoritesList.querySelector(`[data-id="${id}"]`) !== null;
+
+      if (isLiked) { // если уже лайкнуто (товар есть в списке избранного)
+        removeProduct(id); // удаляем товар, передавая его id
+
+        // обновляем кнопку лайка, по дефолту state будет false, поэтому передаем 1 параметр, но можно и явно - updateButton(button, false)
+        // после этого завершаем работу с текущим элементом через return
+        return updateButton(button);
+      }
+
+      // тут мы знаем, что товар не был лайкнут (isLiked === false), поэтому добавляем его в избранное
+
+      addProduct(data); // передаем объект с данными о товаре
+      return updateButton(button, true); // state - true, кнопка должна быть красной
     });
   });
 });
